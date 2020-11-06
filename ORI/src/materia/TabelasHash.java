b5c56b6eb404ac9da3980708d2b2581ecc0ea2a2
Leonardo Martins <leonardo_ignacio2010@hotmail.com>
2020-11-05 21:47:01 -0300

    atualização

--- ORI/src/materia/TabelasHash.java
+++ ORI/src/materia/TabelasHash.java
@@ -3,7 +3,7 @@ package materia;
 public class TabelasHash {
 
 	public static void main(String[] args) {
-		int resto;
+		int resto, target;
 		final int ordem = 11;
 		int[][] matriz = new int[ordem][ordem];
 		int numeros[] = {225, 216, 918, 589, 899, 503, 552, 567, 591, 444, 343, 628, 240, 77, 421, 637, 380};
@@ -22,6 +22,18 @@ public class TabelasHash {
 			System.out.println();
 			for (int j = 0; j < ordem; j ++) {
 				System.out.print(matriz[i][j] + "\t");
+			}
+		}
+		
+		target = 380;
+		resto = target % ordem;
+		for (int j = 0; j < ordem; j ++) {
+			if (matriz[resto][j] == target) {
+				System.out.print("\n\nO número " + target + " foi encontrado na linha " + (resto+1) + ", coluna " + (j+1));
+				break;
+			}
+			if (j == (ordem-1)) {
+				System.out.print("\n\nO número " + target + " não foi encontrado na matriz!");
 			}
 		}
 	}
